b7f233456fe0b93a48a8f7cae7a27812af246555
Trevor Ogg <trevorogg@Trevors-MacBook-Air.local>
2018-05-12 21:30:24 -0600

    database with burgers

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Cheeseburger', false);
